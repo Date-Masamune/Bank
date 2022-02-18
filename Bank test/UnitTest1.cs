98e8abb3c85d5a063e0ccd36dacee285bfc8a16e
jacob gishwiller <sirbruce13@yahoo.com>
2022-02-18 11:33:32 -0700

    unit testing

---
+++ Bank test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bank_test
+{
+   [TestClass]
+   public class UnitTest1
+   {
+      [TestMethod]
+      public void TestMethod1()
+      {
+
+
+      }
+   }
+}
